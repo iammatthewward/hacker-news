876f9f6830042e0804f308a90ce3acb7a0e2ff3b
iammatthewward@gmail.com <iammatthewward@gmail.com>
2018-03-24 14:31:44 +0000

    feat(app): add top-stores to main app

--- src/client/app.js
+++ src/client/app.js
@@ -2,12 +2,18 @@ import React from 'react';
 import 'normalize.css';
 
 import AppHeader from 'Components/app-header';
+import NewsItemList from 'Components/news-item-list';
+import TopStories from 'Providers/top-stories';
 
 import './app.css';
 
 const App = () => (
     <main className='app'>
         <AppHeader />
+        <TopStories render={({ items }) =>
+            <NewsItemList items={items} />
+        }
+        />
     </main>
 );
 
